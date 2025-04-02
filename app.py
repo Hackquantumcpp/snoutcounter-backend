0f208c0c0efc99ed1c0266c3172005fa10d71cec
Adhvaith <adhvaith.thurvas@gmail.com>
2025-04-01 17:01:39 -0700

    Styling

--- app.py
+++ app.py
@@ -5,15 +5,49 @@ from keys import API_KEY, APPROVAL_CHART_KEY, APPROVAL_TABLE_KEY
 
 # dw = Datawrapper(API_KEY)
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.ZEPHYR])
 
 embed_code_approval = f'<iframe src="https://datawrapper.dwcdn.net/{APPROVAL_CHART_KEY}/" frameborder="0" style="width:100%;height:400px;"></iframe>'
 approval_chart_version = 2
 
-app.title = "Datawrapper Chart Embed Example"
+app.title = "SnoutCounter"
 
-app.layout = html.Div([
-    html.H1("SnoutCounter"),
+app.layout = html.Div(children=[
+    dbc.NavbarSimple(
+        dbc.Container(
+            [
+                html.A(
+                    dbc.Row(children=[
+                        dbc.NavbarBrand(
+                        html.Img(
+                            src='/assets/snoutcounter.png',
+                            height="75px",
+                            width="162px"
+                        ), href="/"
+                    )
+                    ], align='center')
+                ),
+                dbc.Nav(
+                    [
+                        dbc.NavItem(dbc.NavLink("Home", href="/")),
+                        dbc.NavItem(dbc.NavLink("About", href="/")),
+                    ], navbar=True
+                )
+            ]
+        )
+    ),
+    # html.Div(children=[html.Img(
+    #         src="/assets/snoutcounter.png",
+    #         style={
+    #             "display": "block",
+    #             "marginRight": "auto",
+    #             "width":"162px",
+    #             "height":"75px",
+    #         }
+    #     )], style={
+    #         "justify-content": "left",
+    #     }
+    # ),
     html.Div([
         html.Div([
             html.Iframe(
